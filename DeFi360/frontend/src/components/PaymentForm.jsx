03303ed21922a466afcfc0114e50ac5d884ec0e8
sBenitesF77 <73630608+Yominic@users.noreply.github.com>
2026-05-24 10:51:01 -0500

    handle change

---
+++ DeFi360/frontend/src/components/PaymentForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import { MockPaymentProcessor } from "../services/mock/PaymentMock";
+
+function PaymentForm({ currDebt, colValue }) {
+    const [amount, setAmount] = useState("");
+    const [error, setError] = useState("");
+    const [ltvPreview, setLtvPreview] = useState(null);
+    const paymentProcessor = new MockPaymentProcessor();
+
+    const validate = (value) => {
+        if (value <= 0) return "Monto menor a 0";
+        if (value > currDebt) return "Monto no puede superar deuda";
+        return "";
+    };
+
+    const handleChange = async (e) => {
+        const value = parseFloat(e.target.value);
+        setAmount(value);
+
+        const validationError = validate(value);
+        setError(validationError);
+        if (!validationError) {
+            const ltv = await paymentProcessor.simulateLTV({
+                amount: value,
+                currDebt,
+                colValue,
+            });
+            setLtvPreview(ltv);
+        }else{
+            setLtvPreview(null);
+        }
+    };
+}
